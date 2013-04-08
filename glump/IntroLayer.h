77ae433a9b7d34106a1dd55bcdc9a3ec283c9778
brooke <brooke@tinyco.com>
2013-04-08 12:48:35 -0700

    Initial Commit

---
+++ glump/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  glump
+//
+//  Created by Brooke Costa on 4/8/13.
+//  Copyright Brooke Costa 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
